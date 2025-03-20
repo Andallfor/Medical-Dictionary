49f314556a8522cd31c613359afba51c86e54bec
Andallfor <leozwang2005@gmail.com>
2025-03-19 20:25:40 -0400

    symbol table

--- src/app/settings/editor.tsx
+++ src/app/settings/editor.tsx
@@ -1,13 +1,14 @@
-import { ChangeEvent, KeyboardEventHandler, useRef, useState } from "react";
+import { ChangeEvent, KeyboardEventHandler, use, useEffect, useRef, useState } from "react";
 
 function SymbolPicker({ update }: { update: (s: string) => void }) {
     const vowels = [['i', 'ɪ', 'e', 'ɛ', 'ə', 'ʌ', 'əː'], ['u', 'ʊ', 'o', 'ɔ', 'ɔr', 'a', 'ar'], ['aɪ', 'ɔɪ', 'aʊ', 'iɚ', 'ɔr', 'ɛɚ', 'ʊɚ']];
     const consonants = [['th̥', 'th̬', 'ˌ', 'ˈ']];
+    const diacritics = [['́', '̴', '̄']] // https://symbl.cc/en/unicode-table/#combining-diacritical-marks
 
     function map(table: string[][]) {
         return table.map((x, i) => 
             <div key={i} className="flex gap-1 my-1">
-                {x.map((y, k) => <button key={k} className="button" onClick={() => update(y)}>{y}</button>)}
+                {x.map((y, k) => <button key={k} className="button" onMouseDown={() => update(y)}>{y}</button>)}
             </div>);
     }
 
@@ -17,49 +18,49 @@ function SymbolPicker({ update }: { update: (s: string) => void }) {
             {map(vowels)}
             <div className="mt-2 font-semibold">Consonants/Misc.</div>
             {map(consonants)}
+            <div className="mt-2 font-semibold">Diacritics</div>
+            {map(diacritics)}
         </div>
     );
 }
 
-function Line({ index, data, update }: { index: number, data: lineData, update: (index: number, d: lineEditData, isSubmit: boolean) => void }) {
+function Line({ index, data, fn }: { index: number, data: lineData, fn: lineFn }) {
     const wordRef = useRef<HTMLInputElement>(null);
     const pronRef = useRef<HTMLInputElement>(null);
 
     function wrapWord(e: ChangeEvent<HTMLInputElement>) {
-        update(index, {word: e.target.value, pron: data.edit.pron}, false);
+        fn.update(index, {word: e.target.value, pron: data.edit.pron}, false);
     }
 
     function wrapPron(e: ChangeEvent<HTMLInputElement>) {
-        update(index, {word: data.edit.word, pron: e.target.value}, false);
+        fn.update(index, {word: data.edit.word, pron: e.target.value}, true);
     }
 
     function trySubmit(k: React.KeyboardEvent<HTMLInputElement>) {
         if (k.key == 'Enter') {
-            update(index, data.edit, true);
-
             wordRef.current?.blur();
             pronRef.current?.blur();
         }
-
-        k.preventDefault();
-    }
-
-    function notify() {
-        update(index, data.edit, false);
     }
 
     return (
         <div className="flex gap-2 h-7">
-            <div className={"line hover:bg-surface10 " + (data.selected ? 'outline outline-1 outline-green-200' : '')}>
-                <input className="w-1/2 bg-surface10 outline-none placeholder:italic" placeholder="Enter Word"
-                    onChange={wrapWord} value={data.edit.word} onKeyDown={trySubmit} ref={wordRef} onFocus={notify}/>
-                <input className="w-1/2 bg-surface10 outline-none placeholder:italic" placeholder="Pronunciation"
-                    onChange={wrapPron} value={data.edit.pron} onKeyDown={trySubmit} ref={pronRef} onFocus={notify}/>
+            <div className="line hover:bg-surface10">
+                <input className="w-1/2 bg-surface10 outline-none placeholder:italic" placeholder="Enter Word" ref={wordRef} value={data.edit.word}
+                    onFocus={() => fn.setFocus(index, false)} onChange={wrapWord} onKeyDown={trySubmit} onBlur={() => fn.clearSelected(wordRef.current ?? undefined)}/>
+                <input className="w-1/2 bg-surface10 outline-none placeholder:italic" placeholder="Pronunciation" ref={pronRef} value={data.edit.pron}
+                    onFocus={() => fn.setFocus(index, true)} onChange={wrapPron} onKeyDown={trySubmit} onBlur={() => fn.clearSelected(pronRef.current ?? undefined)}/>
             </div>
-            <button className="ri-reset-right-line button"></button>
-            <button className="ri-close-line button disabled:bg-tonal0 disabled:cursor-not-allowed" onClick={() => update(index, {word: '', pron: ''}, true)}></button>
+            <button className="ri-file-search-line button"></button>
+            <button className="ri-close-line button"></button>
         </div>
     );
+}
+
+interface lineFn {
+    update: (index: number, d: lineEditData, isPron: boolean) => void;
+    clearSelected: (s: HTMLInputElement | undefined) => void;
+    setFocus: (index: number, isPron: boolean) => void;
 }
 
 interface lineEditData {
@@ -69,7 +70,11 @@ interface lineEditData {
 
 interface lineData {
     edit: lineEditData;
-    selected: boolean;
+}
+
+interface lineSelection {
+    index: number;
+    isPron: boolean;
 }
 
 function emptyLine(): lineData {
@@ -78,43 +83,46 @@ function emptyLine(): lineData {
             word: '',
             pron: '',
         },
-        selected: false,
     };
 }
 
 export function DictionaryEditor() {
     const [lines, setLines] = useState<lineData[]>([emptyLine()]);
-    const [selected, setSelected] = useState(-1);
+    const [selected, setSelected] = useState<lineSelection>({index: -1, isPron: false});
+    const [preventSelectionClear, setPrevention] = useState(false);
 
     function isEmpty(l: lineEditData) { return l.pron == '' && l.word == ''; }
 
-    function setLine(index: number, data: lineEditData, isSubmit = false) {
-        let l = [...lines];
+    function setLine(index: number, data: lineEditData, isPron: boolean) {
+        console.log('passed');
+        const l = [...lines];
         l[index].edit = data;
 
-        let _selected = selected;
+        if (isEmpty(data) && index != l.length - 1) l.splice(index, 1);
+        if (index == lines.length - 1 && !isEmpty(data)) l.push(emptyLine()); // editing last line should auto add new one
 
-        if (isSubmit) {
-            // editing last line should auto add new one
-            if (index == lines.length - 1 && !isEmpty(data)) l.push(emptyLine());
-            if (isEmpty(data) && index != l.length - 1) l.splice(index, 1);
-
-            _selected = -1;
-        } else if (!isEmpty(data)) _selected = index;
-
-        for (let i = 0; i < l.length; i++) {
-            l[i].selected = _selected == i;
-        }
-
-        setSelected(_selected);
         setLines(l);
+        setSelected({index: index, isPron: isPron});
+    }
+
+    function clearSelected(self: HTMLInputElement | undefined) {
+        if (preventSelectionClear) {
+            setPrevention(false);
+            setTimeout(() => self?.focus(), 10);
+        } else {
+            setSelected({index: -1, isPron: false});
+        }
     }
 
     function addSymbol(symbol: string) {
-        if (selected != -1) {
+        if (selected.index != -1) {
             const l = [...lines];
-            l[selected].edit.pron += symbol;
+            if (selected.isPron) l[selected.index].edit.pron += symbol;
+            else l[selected.index].edit.word += symbol;
 
+            if (selected.index == lines.length - 1) l.push(emptyLine()); // editing last line should auto add new one
+
+            setPrevention(true);
             setLines(l);
         }
     }
@@ -126,7 +134,11 @@ export function DictionaryEditor() {
                 <div className="bg-surface20 w-[2px] mx-2"></div>
                 <div className="flex-grow flex flex-col gap-2">
                     {lines.map((x, k) =>
-                        <Line key={k} index={k} data={x} update={setLine}/>
+                        <Line key={k} index={k} data={x} fn={{
+                            update: setLine,
+                            clearSelected: clearSelected,
+                            setFocus: (i, p) => setSelected({index: i, isPron: p})
+                        }}/>
                     )}
                 </div>
                 <div className="w-8"></div>
